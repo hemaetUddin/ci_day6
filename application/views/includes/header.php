8d736a83c6f1db7b06fa7ce8aa87178eeaa78433
Rinto Sarkar <rinto.sp@gmail.com>
2015-02-23 18:05:07 +0600

    First Commmit for ci_day6

---
+++ application/views/includes/header.php
@@ -0,0 +1,10 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>css/style.css">
+</head>
+<body>
+	
